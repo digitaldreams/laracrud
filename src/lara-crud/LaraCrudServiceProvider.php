abf756b20032aaeb02dabb06ce597c6342f8561a
Tuhin <digitaldreams40@gmail.com>
2016-09-17 10:29:44 +0000

    service proiver added

---
+++ src/lara-crud/LaraCrudServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace LaraCrud;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Description of LaraCrudServiceProvider
+ *
+ * @author Tuhin
+ */
+class LaraCrudServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/laracrud.php' => config_path('laracrud.php')
+        ]);
+    }
+
+    public function register()
+    {
+
+    }
+}
