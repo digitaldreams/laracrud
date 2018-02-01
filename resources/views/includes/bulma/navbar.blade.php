2810d95778d8122d949a148915dbedebb72c74f2
Tuhin Bepari <digitaldreams40@gmail.com>
2018-02-01 14:40:27 +0000

    Adding bulma view

--- resources/views/includes/bulma/navbar.blade.php
+++ resources/views/includes/bulma/navbar.blade.php
@@ -2,8 +2,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png"
-                     alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                <img src="assets/logo.png" alt="{{config('app.name')}}" class="image is-48x48">
             </a>
 
             <button class="button navbar-burger" data-target="navMenu">
--- resources/views/layouts/bulma.blade.php
+++ resources/views/layouts/bulma.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title</title>
-    <link rel="stylesheet" href="{{asset('css/bulma.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>{{config('app.name')}}</title>
+    <link rel="stylesheet" href="{{asset('laracrud/css/bulma.css')}}">
+    <link href="{{ asset('laracrud/css/font-awesome.min.css') }}" rel="stylesheet">
     @section('styles')
 </head>
 <body>
