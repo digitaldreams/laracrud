e32050685c2b6c1522c93f2283ed5d9c889c4fce
Tuhin Bepari <digitaldreams40@gmail.com>
2020-07-10 18:46:35 +0600

    Restore and ForceDelete method added.

---
+++ src/lara-crud/Services/Controller/ForceDelete.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace LaraCrud\Services\Controller;
+
+
+use LaraCrud\Contracts\Controller\RedirectAbleMethod;
+
+class ForceDelete extends Restore
+{
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        $variable = '$' . $this->getModelShortName();
+        $body = $variable . ' = ' . ucfirst($this->getModelShortName()) . '::withTrashed()->where(\'' . $this->model->getRouteKeyName() . '\',' . $variable . ')->firstOrFail()' . PHP_EOL;
+
+        $body .= "\t\t" . $variable . '->forceDelete();' . PHP_EOL;
+
+        return $body;
+    }
+    /**
+     * @return array
+     */
+    public function generateRouteParameter()
+    {
+        $parameters = parent::generateRouteParameter();
+        unset($parameters[$this->getModelShortName()]);
+
+        return $parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function redirectToRouteMethodName(): string
+    {
+        return 'index';
+    }
+
+}
---
+++ src/lara-crud/Services/Controller/Restore.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LaraCrud\Services\Controller;
+
+use LaraCrud\Contracts\Controller\RedirectAbleMethod;
+
+class Restore extends ControllerMethod implements RedirectAbleMethod
+{
+    /**
+     * @return \LaraCrud\Services\Controller\ControllerMethod|void
+     *
+     * @throws \ReflectionException
+     */
+    protected function beforeGenerate()
+    {
+        if ($this->parentModel) {
+            $this->setParameter(ucfirst($this->getParentShortName()), '$' . $this->getParentShortName());
+        }
+        $this->setParameter('int', '$' . $this->getModelShortName());
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        $variable = '$' . $this->getModelShortName();
+        $body = $variable . ' = ' . ucfirst($this->getModelShortName()) . '::withTrashed()->where(\'' . $this->model->getRouteKeyName() . '\',' . $variable . ')->firstOrFail()' . PHP_EOL;
+
+        $body .= "\t\t" . $variable . '->restore();' . PHP_EOL;
+
+        return $body;
+    }
+}
