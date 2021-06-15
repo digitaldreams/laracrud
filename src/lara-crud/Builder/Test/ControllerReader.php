0795d76dac2a7478270ddc7fe67343e39db88a61
Tuhin Bepari <digitaldreams40@gmail.com>
2021-06-15 17:22:41 +0600

    Controller Reader Class completed

--- src/lara-crud/Builder/Test/ControllerReader.php
+++ src/lara-crud/Builder/Test/ControllerReader.php
@@ -4,7 +4,98 @@
 namespace LaraCrud\Builder\Test;
 
 
+use Illuminate\Support\Facades\Route;
+
 class ControllerReader
 {
+    /**
+     * controller Full Namespace.
+     *
+     * @var string
+     */
+    protected string $controller;
+    /**
+     * List of Public method's of this controller
+     *
+     * @var \ReflectionMethod[]
+     */
+    protected array $methods = [];
+
+    protected array $routes = [];
+
+    /**
+     * ControllerReader constructor.
+     *
+     * @param string $controller
+     *
+     * @throws \ReflectionException
+     */
+    public function __construct(string $controller)
+    {
+        $this->controller = $controller;
+        $reflectionClass = new \ReflectionClass($controller);
+        $methods = $reflectionClass->getMethods(\ReflectionMethod::IS_PUBLIC);
+        $this->methods = $this->filterMethod($controller, $methods);
+        $this->routes = $this->parseRoutes();
+    }
+
+    /**
+     * @return array
+     */
+    public function getMethods(): array
+    {
+        return $this->methods;
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * @return array
+     */
+    protected function parseRoutes(): array
+    {
+        $returnRoutes = [];
+
+        $routes = Route::getRoutes();
+        foreach ($routes as $route) {
+            $controllerName = strstr($route->getActionName(), '@', true);
+            if ($this->controller != $controllerName) {
+                continue;
+            }
+            $methodName = str_replace('@', '', strstr($route->getActionName(), '@'));
+            $returnRoutes[$methodName] = [
+                'name' => $route->getName(),
+                'path' => $route->uri(),
+                'method' => $methodName,
+                'http_verbs' => $route->methods(),
+                'parameters' => $route->parameterNames(),
+            ];
+        }
+
+        return $returnRoutes;
+    }
+
+    /**
+     * Child class all the method of its parent. But we will accept only child class method.
+     *
+     * @param string              $controllerName
+     * @param \ReflectionMethod[] $reflectionMethods
+     *
+     * @return array
+     */
+    protected function filterMethod(string $controllerName, array $reflectionMethods)
+    {
+        $retMethods = [];
+        foreach ($reflectionMethods as $method) {
+            if (0 != substr_compare($method->name, '__', 0, 2) && $method->class == $controllerName) {
+                $retMethods[$method->name] = $method;
+            }
+        }
+
+        return $retMethods;
+    }
 
 }
