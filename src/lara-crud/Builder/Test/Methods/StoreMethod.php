4911345ef7faa19b9da76b2ac7e6bb82204f27ce
Tuhin Bepari <digitaldreams40@gmail.com>
2021-06-21 10:21:03 +0600

    Now Unauthentication Post Request can have API endpoints tests

--- src/lara-crud/Builder/Test/Methods/StoreMethod.php
+++ src/lara-crud/Builder/Test/Methods/StoreMethod.php
@@ -13,23 +13,25 @@ class StoreMethod extends ControllerMethod
     {
         if ($this->isAuthRequired()) {
             $this->testMethods[] = (new TemplateManager(
-                'test/api/store/a_user_can_create_a_new_model.txt',
-                array_merge($this->getGlobalVariables(), ['data' => $this->generatePostData()])
-            ));
-            $this->testMethods[] = (new TemplateManager(
                 'test/api/store/a_guest_cannot_create_a_new_model.txt',
                 array_merge($this->getGlobalVariables(), ['data' => $this->generatePostData()])
             ));
-            $this->testMethods[] = (new TemplateManager(
-                'test/api/store/create_new_model_validation_check.txt',
-                $this->getGlobalVariables()
-            ));
-
-            $this->testMethods[] = (new TemplateManager(
-                'test/api/store/createNewModelValidationProvider.txt',
-                array_merge($this->getGlobalVariables(), ['data' => $this->generateDataProvider()])
-            ));
         }
+        
+        $this->testMethods[] = (new TemplateManager(
+            'test/api/store/a_user_can_create_a_new_model.txt',
+            array_merge($this->getGlobalVariables(), ['data' => $this->generatePostData()])
+        ));
+
+        $this->testMethods[] = (new TemplateManager(
+            'test/api/store/create_new_model_validation_check.txt',
+            $this->getGlobalVariables()
+        ));
+
+        $this->testMethods[] = (new TemplateManager(
+            'test/api/store/createNewModelValidationProvider.txt',
+            array_merge($this->getGlobalVariables(), ['data' => $this->generateDataProvider()])
+        ));
     }
 
 }
