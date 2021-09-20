65eb353b2bde96fef74118d76b6a92ff3345183d
Tuhin Bepari <digitaldreams40@gmail.com>
2021-09-20 08:05:55 +0600

    Working with react Form

--- src/lara-crud/Crud/ReactJs/ReactJsFormCrud.php
+++ src/lara-crud/Crud/ReactJs/ReactJsFormCrud.php
@@ -5,7 +5,10 @@ namespace LaraCrud\Crud\ReactJs;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Model;
 use LaraCrud\Contracts\Crud;
+use LaraCrud\Contracts\TableContract;
 use LaraCrud\Helpers\TemplateManager;
+use LaraCrud\Services\ControllerMethodReader;
+use LaraCrud\Services\ControllerReader;
 
 class ReactJsFormCrud implements Crud
 {
@@ -32,11 +35,12 @@ class ReactJsFormCrud implements Crud
         $this->model = $model;
         $this->controller = $controller;
         $this->componentName = (new \ReflectionClass($this->model))->getShortName() . 'Form';
-
     }
 
     public function template()
     {
+        print_r($this->getValidationRules());
+
         return (new TemplateManager('reactjs/form.txt', [
             'componentName' => $this->componentName,
             'validationRules' => '',
@@ -50,5 +54,36 @@ class ReactJsFormCrud implements Crud
         $fullPath = config('laracrud.reactjs.rootPath') . '/components/forms/' . $this->componentName . '.js';
         $migrationFile = new \SplFileObject($fullPath, 'w+');
         $migrationFile->fwrite($this->template());
+    }
+
+    protected function generateFields(): string
+    {
+
+    }
+
+    protected function getValidationRules(): array
+    {
+        $controllerReader = new ControllerReader(get_class($this->controller));
+        $methods = $controllerReader->getMethods();
+        $routes = $controllerReader->getRoutes();
+
+        foreach ($methods as $name => $reflectionMethod) {
+            if (in_array($name, ['store', '__invoke'])) {
+                $methodReader = new ControllerMethodReader($reflectionMethod, $routes[$name]);
+
+                return $methodReader->getCustomRequestClassRules();
+            }
+        }
+    }
+
+    protected function fetchColumnClasses(): array
+    {
+        $retColumn = [];
+        $tableRepository = app()->make(TableContract::class, ['table' => $this->model->getTable()]);
+        foreach ($tableRepository->columns() as $column) {
+            $retColumn[$column->name()] = $column;
+        }
+
+        return $retColumn;
     }
 }
